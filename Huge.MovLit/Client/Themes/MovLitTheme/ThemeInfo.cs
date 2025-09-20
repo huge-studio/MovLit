8b0274dccead13e25bb4ce389afe7777f8baa3d1
Mark Davis <github@nohorse.com>
2025-09-20 16:03:28 -0700

    Renames "MyTheme" to "MovLitTheme"
    Updates the theme name and associated resources from "MyTheme" to "MovLitTheme" for consistency and clarity.
    
    This includes renaming directories, namespaces, resource files, and updating theme information within the Oqtane CMS.
    
    Also, modifies the Ink module to process all available lines from the story at once.
    
    Adds a default connection string in appsettings.json.

--- Huge.MovLit/Client/Themes/MovLitTheme/ThemeInfo.cs
+++ Huge.MovLit/Client/Themes/MovLitTheme/ThemeInfo.cs
@@ -3,17 +3,17 @@ using Oqtane.Models;
 using Oqtane.Themes;
 using Oqtane.Shared;
 
-namespace Huge.MovLit.MyTheme
+namespace Huge.MovLit.MovLitTheme
 {
     public class ThemeInfo : ITheme
     {
         public Oqtane.Models.Theme Theme => new Oqtane.Models.Theme
         {
-            Name = "MyTheme",
+            Name = "MovLitTheme",
             Version = "1.0.0",
             PackageName = "Huge.MovLit",
-            ThemeSettingsType = "Huge.MovLit.MyTheme.ThemeSettings, Huge.MovLit.Client.Oqtane",
-            ContainerSettingsType = "Huge.MovLit.MyTheme.ContainerSettings, Huge.MovLit.Client.Oqtane",
+            ThemeSettingsType = "Huge.MovLit.MovLitTheme.ThemeSettings, Huge.MovLit.Client.Oqtane",
+            ContainerSettingsType = "Huge.MovLit.MovLitTheme.ContainerSettings, Huge.MovLit.Client.Oqtane",
             Resources = new List<Resource>()
             {
                 new Stylesheet(Constants.BootstrapStylesheetUrl, Constants.BootstrapStylesheetIntegrity, "anonymous"),
