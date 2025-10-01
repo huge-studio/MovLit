a81a3ea325bae62978ff6c737542edc671655986
Bryce Fisher <bryce.e.fisher@gmail.com>
2025-10-01 08:07:45 -0700

    Enhances Lottie/Image settings UI
    Improves user experience in Lottie and Image source settings.
    
    Adds help text to guide users on source type selection and
    source input.
    
    Introduces URL input placeholders for clarity and adjusts layout
    for improved visual consistency.

--- Huge.MovLit/Client/Modules/Lottie/Settings.razor.cs
+++ Huge.MovLit/Client/Modules/Lottie/Settings.razor.cs
@@ -5,8 +5,6 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using Oqtane.Modules;
 using Oqtane.Services;
-using Oqtane.Models;
-using Oqtane.Shared;
 using Huge.MoveLit.Enums;
 
 
