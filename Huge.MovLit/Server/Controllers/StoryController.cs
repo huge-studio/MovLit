6c901ba95d89b3e6c3830f2f2fed0743338f1cba
Bryce Fisher <bryce.e.fisher@gmail.com>
2025-11-10 06:47:48 -0800

    Updates error handling for story retrieval
    Improves the handling of unauthorized story retrieval attempts by returning a 404 Not Found status code instead of a 403 Forbidden.
    
    This provides a more appropriate response when a user attempts to access a story that doesn't exist or is not available to them. Updates the logging message to reflect the failed attempt.

--- Huge.MovLit/Server/Controllers/StoryController.cs
+++ Huge.MovLit/Server/Controllers/StoryController.cs
@@ -105,8 +105,8 @@ namespace Huge.MovLit.Controllers
             }
             else
             {
-                _logger.Log(LogLevel.Error, this, LogFunction.Security, "Unauthorized Story Get Attempt {StoryId} {ModuleId}", id, moduleid);
-                HttpContext.Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                _logger.Log(LogLevel.Error, this, LogFunction.Security, "Failed Story Get Attempt {StoryId} {ModuleId}", id, moduleid);
+                HttpContext.Response.StatusCode = (int)System.Net.HttpStatusCode.NotFound;
                 return null;
             }
         }
