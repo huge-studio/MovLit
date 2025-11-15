ecabc7947a20b29fb5ca9b122c2f3de532c615c5
Bryce Fisher <bryce.e.fisher@gmail.com>
2025-11-15 13:08:13 -0800

    Adds Uploader module for file management
    Introduces a new module to handle file uploads,
    allowing users to manage images and Lottie files.
    
    Removes the initial URL setting logic from the Ink module
    as it is no longer needed.
    
    Simplifies Lottie module settings retrieval from SiteState.

---
+++ Huge.MovLit/Client/Modules/Uploader/Index.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+using Oqtane.Modules;
+using Oqtane.Services;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Huge.Uploader
+{
+    public partial class Index : ModuleBase
+    {
+        [Inject] public ISettingService SettingService { get; set; }
+
+        private string _returnUrl;
+        private string _settingsUrl;
+
+        protected override void OnParametersSet()
+        {
+            _returnUrl = WebUtility.UrlEncode(PageState.Uri.AbsolutePath.ToString());
+            _settingsUrl = EditUrl("Settings", $"returnurl={_returnUrl}&tab=ModuleSettings");
+        }
+    }
+}
